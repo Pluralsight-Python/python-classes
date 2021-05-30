7b96b91c5f01fcc8423090d617f4a963e9fa214c
Nishant Misra <nishant.mnnit2k9@gmail.com>
2021-05-30 16:34:59 +0530

    Base code for learning about Class Decorators

---
+++ class_decorators/location.py
@@ -0,0 +1,27 @@
+"""
+    Practice file for understanding and learning about Class Decorators
+"""
+from string_representations import Position, EarthPosition, MarsPosition
+
+
+class Location:
+
+    def __init__(self, name, position):
+        self._name = name
+        self._position = position
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def position(self):
+        return self._position
+
+
+hong_kong = Location('Hong Kong', EarthPosition(latitude=22.29, longitude=114.16))
+stockholm = Location('Stockholm', EarthPosition(latitude=59.33, longitude=18.06))
+cape_town = Location('Cape Town', EarthPosition(latitude=-33.93, longitude=18.42))
+rotterdam = Location('Rotterdam', EarthPosition(latitude=51.96, longitude=4.47))
+maracaibo = Location('Maracaibo', EarthPosition(latitude=10.65, longitude=-71.65))
+
