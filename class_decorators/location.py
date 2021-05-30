f7bd7924cdffd90ab5a6ebc74e2b6d9e61d3fba8
Nishant Misra <nishant.mnnit2k9@gmail.com>
2021-05-30 21:07:18 +0530

    Add basic __repr__ and __str__

--- class_decorators/location.py
+++ class_decorators/location.py
@@ -1,7 +1,7 @@
 """
     Practice file for understanding and learning about Class Decorators
 """
-from string_representations import Position, EarthPosition, MarsPosition
+from string_representations import Position, EarthPosition, MarsPosition, typename
 
 
 class Location:
@@ -17,6 +17,13 @@ class Location:
     @property
     def position(self):
         return self._position
+
+    def __repr__(self):
+        # Returning a constructor-like representation
+        return f'{typename(self)}(name={self.name}, position={self.position})'
+
+    def __str__(self):
+        return self.name
 
 
 hong_kong = Location('Hong Kong', EarthPosition(latitude=22.29, longitude=114.16))
