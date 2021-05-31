6592d7d1dc2bc9d0f8f6e22ce0cb160e3516246d
Nishant Misra <nishant.mnnit2k9@gmail.com>
2021-05-31 23:49:26 +0530

    Base Code for applying class decorator factories

---
+++ class_decorators/itinerary.py
@@ -0,0 +1,30 @@
+"""
+    Another example of class decorator, this time using class decorator factory
+"""
+
+
+class Itinerary:
+
+    def __init__(self, locations):
+        self._locations = list(locations)
+
+    @classmethod
+    def from_args(cls, *locations):
+        return cls(locations)
+
+    def __str__(self):
+        return '\n'.join([location.name for location in self._locations])
+
+    @property
+    def locations(self):
+        return self._locations
+
+    def add(self, location):
+        self.locations.append(location)
+
+    def remove(self, location):
+        self.locations.remove(location)
+
+    def truncate_at(self, location):
+        index = self.locations.index(location)
+        self._locations = self._locations[:index + 1]
