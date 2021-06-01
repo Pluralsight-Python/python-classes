793ab1007f26872dea9ae03bfab43d49e34cbcfb
Nishant Misra <nishant.mnnit2k9@gmail.com>
2021-06-01 21:07:49 +0530

    Generate more Invariant Decorators
    Also use multiple decorators

--- class_decorators/itinerary.py
+++ class_decorators/itinerary.py
@@ -2,7 +2,7 @@
     Another example of class decorator, this time using class decorator factory
 """
 import functools
-from .location import *
+from class_decorators.location import *
 
 
 # A function decorator factory as a starting point for class decorator factory
@@ -14,7 +14,8 @@ def post_condition(predicate):
             ret = func(_self, *args, **kwargs)
             if not predicate(_self):
                 raise RuntimeError(
-                    f"The predicate '{predicate.__name__}' failed for function {func.__name__}"
+                    f"The predicate '{predicate.__name__}' failed for function '{func.__name__}(...)' "
+                    f"of class {typename(_self)}"
                 )
             return ret
         return wrapper
@@ -38,11 +39,18 @@ def invariant(predicate):
     return class_decor
 
 
+# This predicate shall raise error on duplicates
+def no_duplicates(_self):
+    return len(set(_self._locations)) == len(_self._locations)
+
+
 # This predicate shall check if the itenerary has at least 2 locations
 def at_least_two_locations(_self):
     return len(_self._locations) >= 2
 
 
+@class_decorator_auto_repr
+@invariant(no_duplicates)
 @invariant(at_least_two_locations)
 class Itinerary:
 
